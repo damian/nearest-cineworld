234efa4b61314611ef035db951e46988037e7543
Damian Nicholson <damian.nicholson@cef.co.uk>
2011-04-03 23:30:25 +0100

    Removed duplicate migration

--- db/migrate/20110325121922_add_slug_to_cinemas.rb
+++
@@ -1,9 +0,0 @@
-class AddSlugToCinemas < ActiveRecord::Migration
-  def self.up
-    add_column :cinemas, :cached_slug, :string
-  end
-
-  def self.down
-    remove_column :cinemas, :cached_slug
-  end
-end
