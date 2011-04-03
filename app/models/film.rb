0ef9897443a3895f3ce70848f5bfd8a5cf6717aa
Damian Nicholson <damian.nicholson@cef.co.uk>
2011-04-03 23:56:06 +0100

    Changed the group by for heroku

--- app/models/film.rb
+++ app/models/film.rb
@@ -15,7 +15,7 @@ class Film < ActiveRecord::Base
 
   # Scopes
   default_scope order('title asc')
-  scope :grouped, group('films.edi, films.id, films.title, films.poster_url, films.film_url')
+  scope :grouped, group('films.edi, films.id, films.title, films.poster_url, films.film_url, films.still_url, films.advisory, films.classification')
   scope :performances_for_date, lambda { |date| where('performances.date = ?', date) }
   scope :today, performances_for_date(Date.today)
   scope :tomorrow, performances_for_date(Date.today + 1)
